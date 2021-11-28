3fe04f17eb423e9c9c0ac0042433306e648a4856
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-11-28 10:08:12 +0100

    more views done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
